3c828c930215ea516437f42b1651b4bfc17f5d19
Katherine Worley <kdesign216@yahoo.com>
2018-05-25 08:49:23 -0400

    added edit and delete link to show notes page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -23,12 +23,20 @@ class NotesController < ApplicationController
 	end 
 
 	def edit
+
 	end 
 
 	def update
+		if @note.update(note_params)
+			redirect_to @note
+		else
+			render 'edit'
+		end 
 	end 
 
 	def destroy
+		@note.destroy
+		redirect_to notes_path
 	end
 
 	private
