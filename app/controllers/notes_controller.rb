96a42688ca763457053ae407b492afa81ae912b8
Katherine Worley <kdesign216@yahoo.com>
2018-05-25 08:08:29 -0400

    added method for create and before action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+	before_action :find_note, only: [:show, :edit, :update, :destroy]
+
 	def index
 	end
 
@@ -11,6 +13,12 @@ class NotesController < ApplicationController
 
 	def create
 		@note = Note.new(note_params)
+
+		if @note.save
+			redirect_to @note
+		else
+			render 'new'
+		end 
 	end 
 
 	def edit
@@ -25,9 +33,11 @@ class NotesController < ApplicationController
 	private
 
 	def find_note
+		@note = Note.find(params[:id])
 	end
 
 	def note_params
 		params.require(:note).permit(:title, :content)
 	end
 end
+
