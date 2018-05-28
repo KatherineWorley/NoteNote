b2a16c941ac8aac5630568ff06eacb55148f711f
Katherine Worley <kdesign216@yahoo.com>
2018-05-28 07:08:39 -0400

    generated migration for user id and built assocation of users with many notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_131815) do
+ActiveRecord::Schema.define(version: 2018_05_28_110450) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
