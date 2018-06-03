25c00a1b26c793ec6d694de95af3bc76eed2b912
Katherine Worley <kdesign216@yahoo.com>
2018-06-03 06:13:03 -0400

    added authenticated root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :notes 
+
+  authenticated :user do 
+  	root 'notes#index', as "authenticated_root"
+  end
 
   root 'welcome#index'
-
-  resources :notes 
   
 end
